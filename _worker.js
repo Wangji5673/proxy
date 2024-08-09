f329a7dde9dde46d9fe308bb8cc66f15c0adea00
Wangji5673 <116420705+Wangji5673@users.noreply.github.com>
2024-08-09 19:10:19 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '0bf21e03-7822-4d71-8ce7-1a4d2d58d2b5';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
